387d4fc98baddefe3380fcfad190906bbfd572bf
Iggi <iggirex@gmail.com>
2018-03-01 15:15:14 -0700

    finished actions and reducers

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item">{book.title} </li>
+                <li 
+                    key={book.title}
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
+                    {book.title} 
+                </li>
             )
         })
     }
@@ -19,27 +26,14 @@ class BookList extends Component {
     }
 }
 
-// mapStateToProps is what connects React to Redux
-// whenever application state changes, this 
-// container (BookList) will rerender
 function mapStateToProps(state) {
-    // Whatever is returned will show up as props insed BookList
     return {
         books: state.books
     };
 }
 
-// HERE WE USE CONNECT FUNCTION WE IMPORTED
-// connect is what says "take this component, take this 
-// mapStateToProps" and RETURN A CONTAINER
-// whenever we make a container we don't want to export 
-// BookList class,
-// we want to export the container
-export default connect(mapStateToProps)(BookList);
-// connect takes a function and a component and
-// produces a container
-// container => a component that is aware of the state that is
-// contained by redux
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
 
-// MapStateToProps => returns an object which is available as
-// this.state.props
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
